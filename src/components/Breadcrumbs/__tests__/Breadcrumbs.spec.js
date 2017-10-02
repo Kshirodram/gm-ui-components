ea6fa3bc43847257bfc7644a8f1a904bda6f8bf2
David Tillery <tilleryd@gmail.com>
2017-10-02 17:38:48 -0400

    Move tests into __tests__ folders.

--- src/components/Breadcrumbs/__tests__/Breadcrumbs.spec.js
+++ src/components/Breadcrumbs/__tests__/Breadcrumbs.spec.js
@@ -1,10 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-import renderer from "react-test-renderer";
-import { Breadcrumbs, BreadcrumbItem } from "./index.js";
-import { Elipsis } from "./Breadcrumbs";
-import { mount, shallow } from "enzyme";
-import "jest-styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
+import { Breadcrumbs, BreadcrumbItem } from '../index.js';
+import { Elipsis } from '../Breadcrumbs';
+import { mount, shallow } from 'enzyme';
+import 'jest-styled-components';
 
 const TestExpanded = (
   <Breadcrumbs maxItems={10}>
@@ -26,16 +26,16 @@ const TestCollapsed = (
 const ExpandedBreadcrumbTree = renderer.create(TestExpanded).toJSON();
 const CollapsedBreadcrumbTree = renderer.create(TestCollapsed).toJSON();
 
-describe("<Breadcrumbs> Snapshot", () => {
-  test("renders expanded breadcrumbs correctly", () => {
+describe('<Breadcrumbs> Snapshot', () => {
+  test('renders expanded breadcrumbs correctly', () => {
     expect(ExpandedBreadcrumbTree).toMatchSnapshot();
   });
-  test("renders collapsed breadcrumbs correctly", () => {
+  test('renders collapsed breadcrumbs correctly', () => {
     expect(CollapsedBreadcrumbTree).toMatchSnapshot();
   });
 });
 
-describe("<Breadcrumbs>", () => {
+describe('<Breadcrumbs>', () => {
   let wrapper, instance;
 
   beforeEach(() => {
@@ -43,22 +43,22 @@ describe("<Breadcrumbs>", () => {
     instance = wrapper.instance();
   });
 
-  test("collapse when children are greater than maxItems", () => {
-    expect(wrapper.state("isCollapsed")).toEqual(true);
+  test('collapse when children are greater than maxItems', () => {
+    expect(wrapper.state('isCollapsed')).toEqual(true);
     expect(wrapper.find(BreadcrumbItem).length).toBe(3);
   });
 
-  test("do not collapse when children are less than maxItems", () => {
+  test('do not collapse when children are less than maxItems', () => {
     wrapper.setProps({ maxItems: 5 });
-    expect(wrapper.state("isCollapsed")).toEqual(false);
+    expect(wrapper.state('isCollapsed')).toEqual(false);
     expect(wrapper.find(BreadcrumbItem).length).toBe(4);
   });
 
-  test("expand when elipsis is clicked", () => {
-    wrapper.find(Elipsis).simulate("click");
-    expect(wrapper.state("isCollapsed")).toEqual(false);
+  test('expand when elipsis is clicked', () => {
+    wrapper.find(Elipsis).simulate('click');
+    expect(wrapper.state('isCollapsed')).toEqual(false);
   });
-  test("a single breadcrumb is not longer than 20 characters", () => {
+  test('a single breadcrumb is not longer than 20 characters', () => {
     wrapper.find(BreadcrumbItem).forEach(item => {
       expect(
         item
