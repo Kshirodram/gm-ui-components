fe453a9f7f4feb68312774e246bf3a256d30c56b
Stuart Hanberg <shanberg@users.noreply.github.com>
2018-09-06 08:12:20 -0400

    feat: add themeprovider decoration (#316)
    * feat: adding themeprovider
    
    * fix: fixing missing imports
    
    * fix: fixing whitespace
    
    * fix: requiring theme addon in the right package group
    
    * fix: removing redundant name from theme object

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,9 @@ import { configure, addDecorator } from "@storybook/react";
 import { setDefaults } from "@storybook/addon-info";
 import { setOptions } from "@storybook/addon-options";
 import { withKnobs } from "@storybook/addon-knobs/react";
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+
+import { keen, keenDark } from 'style/styleVariables';
 
 const req = require.context("../src", true, /\.stories\.js$/);
 
@@ -26,6 +29,8 @@ setDefaults({
   maxPropArrayLength: 10,
   maxPropStringLength: 100
 });
+
+addDecorator(withThemesProvider([keen, keenDark]));
 
 // Options:
 setOptions({
