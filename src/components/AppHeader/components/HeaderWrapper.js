578715e0f8b839d1f4fe7b4b7ad7adef79e78c87
shanberg <shanberg@users.noreply.github.com>
2018-11-06 12:43:00 -0500

    feat: removing unnecessary theming from appheader

--- src/components/AppHeader/components/HeaderWrapper.js
+++ src/components/AppHeader/components/HeaderWrapper.js
@@ -1,17 +1,16 @@
 import styled from "styled-components";
-import { transparentize } from "polished";
 
 import BannerBackgroundImage from "./images/mesh-web.jpg";
 
 const HeaderWrapper = styled.div`
   font-family: ${props => props.theme.FONT_STACK_BASE};
-  color: ${props => props.theme.COLOR_CONTENT};
-  background-color: ${props => props.theme.COLOR_BACKGROUND_C};
+  color: #fff;
+  background-color: #000;
   background-image: linear-gradient(
       to right,
-      ${props => props.theme.COLOR_BACKGROUND_C},
-      ${props => transparentize(0.7, props.theme.COLOR_BACKGROUND_C)},
-      ${props => transparentize(1, props.theme.COLOR_BACKGROUND_C)}
+      #000,
+      rgba(0, 0, 0, 0.3),
+      rgba(0, 0, 0, 0)
     ),
     url(${BannerBackgroundImage});
   display: flex;
